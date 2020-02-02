332e9fbfa518dee522d324d8f656f45a673a519b
Martin Algesten <martin@algesten.se>
2020-02-02 22:01:57 +0100

    agent: doc

--- src/agent.rs
+++ src/agent.rs
@@ -10,39 +10,117 @@ use crate::Connection;
 use crate::Error;
 use crate::ResponseExt;
 
+/// Agents provide redirects, connection pooling, cookies and retries.
+///
+/// Every request is sent through an agent, also when using the extension trait
+/// (`Request.send()`). When using the trait, the agent is intantiated with default
+/// parameters and lives for the length of the request.
+///
+/// To amend the default parameters, or reuse an agent over many requests, use `Agent::new()`
+/// and send the request using `agent.send(req)`.
+///
+/// The default agent have the following settings:
+///
+///   * Redirects: 5
+///   * Retries: 5
+///   * Connection pooling: on
+///   * Cookies: on (TODO)
+///
+/// The settings can be changed, and are used for the next `.send()` call. It is possible
+/// to change the settings between calls.
 #[derive(Default)]
 pub struct Agent {
     connections: Vec<Connection>,
-    retries: usize,
-    redirects: usize,
+    redirects: i8,
+    retries: i8,
+    pooling: bool,
 }
 
 impl Agent {
+    /// Creates a new agent with default parameters.
+    ///
+    /// ```
+    /// use hreq::Agent;
+    ///
+    /// let agent = Agent::new();
+    /// ```
     pub fn new() -> Self {
         Agent {
             connections: vec![],
-            retries: 5,
             redirects: 5,
+            retries: 5,
+            pooling: true,
         }
     }
 
-    pub fn retries(&mut self, retries: usize) {
-        self.retries = retries;
+    /// Changes number of redirects.
+    ///
+    /// Defaults to `5`. Set to `0` to disable redirects.
+    ///
+    /// The number of redirects will be used for the next call to `.send()`.
+    ///
+    /// ```
+    /// use hreq::Agent;
+    ///
+    /// let mut agent = Agent::new();
+    /// agent.redirects(0);
+    /// ```
+    pub fn redirects(&mut self, amount: u8) {
+        self.redirects = amount as i8;
     }
 
-    pub fn redirects(&mut self, redirects: usize) {
-        self.redirects = redirects;
+    /// Changes the number of retry attempts.
+    ///
+    /// Defaults to `5`. Set to `0` to disable retries.
+    ///
+    /// The number of retries will be used for the next call to `.send()`.
+    ///
+    /// ```
+    /// use hreq::Agent;
+    ///
+    /// let mut agent = Agent::new();
+    /// agent.retries(0);
+    /// ```
+    pub fn retries(&mut self, amount: u8) {
+        self.retries = amount as i8;
+    }
+
+    /// Turns connection pooling on or off.
+    ///
+    /// Defaults to `true`. Set to `false` to disable connection pooling.
+    ///
+    /// The setting will be used for the next call to `.send()`.
+    ///
+    /// When set to `false` any existing connection currently pooled will be dropped.
+    ///
+    /// ```
+    /// use hreq::Agent;
+    ///
+    /// let mut agent = Agent::new();
+    /// agent.pooling(false);
+    /// ```
+    pub fn pooling(&mut self, enabled: bool) {
+        self.pooling = enabled;
+        if !enabled {
+            self.connections.clear();
+        }
     }
 
     fn reuse_from_pool(&mut self, uri: &http::Uri) -> Result<Option<&mut Connection>, Error> {
-        trace!("Reuse from pool: {}", uri);
+        if !self.pooling {
+            return Ok(None);
+        }
         let hostport = uri.host_port()?;
         let addr = hostport.to_string();
-        Ok(self
+        let ret = self
             .connections
             .iter_mut()
             // http2 multiplexes over the same connection, http1 needs to finish previous req
-            .find(|c| c.addr() == addr && (c.is_http2() || c.unfinished_requests() == 0)))
+            .find(|c| c.addr() == addr && (c.is_http2() || c.unfinished_requests() == 0));
+        if ret.is_some() {
+            trace!("Reuse from pool: {}", uri);
+        }
+        Ok(ret)
     }
 
     async fn connect_and_pool(
@@ -57,24 +135,51 @@ impl Agent {
         Ok(self.connections.get_mut(idx).unwrap())
     }
 
+    /// Sends a request using this agent.
+    ///
+    /// The parameters configured in the agent are used for the request.
+    ///
+    /// Depending on agent settings, connections are pooled and cookies reused between
+    /// repeated calls to `send()`.
+    ///
+    /// ```
+    /// use hreq::prelude::*;
+    /// use hreq::Agent;
+    ///
+    /// let mut agent = Agent::new();
+    /// agent.retries(0);
+    /// agent.redirects(0);
+    ///
+    /// let req = Request::get("https://fails-badly-yeah")
+    ///     .with_body(()).unwrap();
+    ///
+    /// let res = agent.send(req).block();
+    ///
+    /// assert!(res.is_err());
+    /// assert!(res.unwrap_err().is_io());
+    /// ```
     pub async fn send(&mut self, req: http::Request<Body>) -> Result<http::Response<Body>, Error> {
         // mark start of request
-        let deadline = with_request_params(&req, |params| {
+        let (deadline, force_http2) = with_request_params(&req, |params| {
             params.mark_request_start();
-            params.deadline()
+            (params.deadline(), params.force_http2)
         })
-        .unwrap_or_else(Deadline::inert);
+        .unwrap_or_else(|| (Deadline::inert(), false));
 
         // the request should be time limited regardless of retries. the entire do_send()
         // is wrapped in a ticking timer...
-        deadline.race(self.do_send(req)).await
+        deadline.race(self.do_send(req, force_http2)).await
     }
 
-    async fn do_send(&mut self, req: http::Request<Body>) -> Result<http::Response<Body>, Error> {
+    async fn do_send(
+        &mut self,
+        req: http::Request<Body>,
+        force_http2: bool,
+    ) -> Result<http::Response<Body>, Error> {
         let mut retries = self.retries;
         let mut redirects = self.redirects;
-
-        let force_http2 = with_request_params(&req, |p| p.force_http2).unwrap_or(false);
+        let pooling = self.pooling;
+        let mut unpooled: Option<Connection> = None;
 
         let mut next_req = req;
 
@@ -90,7 +195,15 @@ impl Agent {
             // grab connection for the current request
             let conn = match self.reuse_from_pool(req.uri())? {
                 Some(conn) => conn,
-                None => self.connect_and_pool(req.uri(), force_http2).await?,
+                None => {
+                    if pooling {
+                        self.connect_and_pool(req.uri(), force_http2).await?
+                    } else {
+                        let conn = connect(req.uri(), force_http2).await?;
+                        unpooled.replace(conn);
+                        unpooled.as_mut().unwrap()
+                    }
+                }
             };
 
             match conn.send_request(req).await {
@@ -101,7 +214,7 @@ impl Agent {
                         redirects -= 1;
 
                         // no more redirections. return what we have.
-                        if redirects == 0 {
+                        if redirects < 0 {
                             break Ok(res);
                         }
 
