acd19048b735410c1d863440cd7e2568f07cfe87
Martin Algesten <martin@algesten.se>
2020-02-02 19:35:38 +0100

    req_ext: doc

--- src/req_ext.rs
+++ src/req_ext.rs
@@ -1,3 +1,5 @@
+//! Extension trait for `http::request::Request`
+
 use crate::res_ext::HeaderMapExt;
 use crate::Agent;
 use crate::Body;
@@ -6,15 +8,80 @@ use async_trait::async_trait;
 use http::{Request, Response};
 use std::str::FromStr;
 
+/// Extends [`http::request::Request`] with ergonomic extras for hreq.
+///
+/// These extensions are part of the primary goal of hreq to provide a "User first API".
+///
+/// [`http::request::Request`]: https://docs.rs/http/0.2.0/http/request/struct.Request.html
 #[async_trait]
 pub trait RequestExt {
-    //
-
-    /// Get a header, ignore incorrect header values.
+    /// Quickly read a header value as a `&str`.
+    ///
+    /// A header value can in theory contain any byte value 32 to 255 (inclusive), excluding
+    /// 127 (DEL). That means all possible header values are not representable as a `&str`.
+    ///
+    /// In practice it's incredibly rare for any header value to be outside US-ASCII, which
+    /// means for the vast majority of cases `&str` is fine.
+    ///
+    /// This convenience methods treats header values not representable as ascii as `None`.
+    ///
+    /// ```
+    /// use hreq::prelude::*;
+    ///
+    /// let req = Request::get("https://www.google.com")
+    ///     .header("x-my-head", "whatnow")
+    ///     .with_body(()).unwrap();
+    ///
+    /// let value = req.header("x-my-head").unwrap();
+    ///
+    /// assert_eq!(value, "whatnow");
+    /// ```
     fn header(&self, key: &str) -> Option<&str>;
 
+    /// Quickly parse a header value into _something else_.
+    ///
+    /// Rust fabulous `FromStr` trait means we can quickly parse a value into something else.
+    /// For example, if we know a header `x-req-id` is supposed to have a  numeric 64 bit value
+    /// and we want that number, we can do:
+    ///
+    /// ```
+    /// use hreq::prelude::*;
+    ///
+    /// let req = Request::get("https://my-api")
+    ///     .header("x-req-id", 42)
+    ///     .with_body(()).unwrap();
+    ///
+    /// let req_id: u64 = req.header_as("x-req-id").unwrap();
+    ///
+    /// assert_eq!(req_id, 42);
+    /// ```
     fn header_as<T: FromStr>(&self, key: &str) -> Option<T>;
 
+    /// Send this request.
+    ///
+    /// Note: The type signature of this function is complicated because rust doesn't yet
+    /// support the `async` keyword in traits. You can think of this function as:
+    ///
+    /// ```ignore
+    /// async fn send(self) -> Result<Response<Body>, Error>;
+    /// ```
+    ///
+    /// Creates a default configured [`Agent`] used for this request only. The agent will
+    /// follow redirects and provide some retry-logic for idempotent request methods.
+    ///
+    /// If you need connection pooling over several requests or finer grained control over
+    /// retries or redirects, instantiate an [`Agent`] and send the request through it.
+    ///
+    /// ```
+    /// use hreq::prelude::*;
+    ///
+    /// let req = Request::get("https://www.google.com")
+    ///     .with_body(()).unwrap();
+    ///
+    /// req.send().block();
+    /// ```
+    ///
+    /// [`Agent`]: struct.Agent.html
     async fn send(self) -> Result<Response<Body>, Error>;
 }
 
