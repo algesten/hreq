c4e7231b1246dd6e63e33b1a1084dff4c62ce45d
Martin Algesten <martin@algesten.se>
2020-05-08 10:02:57 +0200

    tokio read write

---
+++ tokio-traits/README.md
@@ -0,0 +1,9 @@
+# tokio-read-write-traits
+
+Re-export tokio async traits to be able to write code that ports
+between future `AsyncRead`/`AsyncWrite` to tokio without bringing
+in all of tokio.
+
+This crate is not maintained by the tokio team.
+
+License: MIT
--- tokio-traits/src/lib.rs
+++ tokio-traits/src/lib.rs
@@ -1,2 +1,6 @@
-/// Re-export tokio async traits
+//! Re-export tokio async traits to be able to write code that ports
+//! between future `AsyncRead`/`AsyncWrite` to tokio without bringing
+//! in all of tokio.
+//!
+//! This crate is not maintained by the tokio team.
 pub use tokio::io::{AsyncRead as TokioAsyncRead, AsyncWrite as TokioAsyncWrite};
