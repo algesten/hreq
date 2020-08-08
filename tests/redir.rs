93ebe0b2f35eeb0688fbeb1370f5a95a84b92cde
Martin Algesten <martin@algesten.se>
2020-08-08 15:49:36 +0200

    Redo 302 redirect test

--- tests/redir.rs
+++ tests/redir.rs
@@ -5,29 +5,30 @@ use hreq::Error;
 fn code_302() -> Result<(), Error> {
     let mut server = Server::new();
 
-    server.at("/path").all(|_: http::Request<Body>| async move {
-        http::Response::builder()
-            .status(302)
-            .header("Location", "https://www.google.com/")
-            .body(())
-            .unwrap()
-    });
+    server
+        .at("/path1")
+        .all(|_: http::Request<Body>| async move {
+            http::Response::builder()
+                .status(302)
+                .header("Location", "/path2")
+                .body(())
+                .unwrap()
+        });
+
+    server
+        .at("/path2")
+        .all(|_: http::Request<Body>| async move { http::Response::builder().body("OK").unwrap() });
 
     let (shut, addr) = server.listen(0).block()?;
 
-    let uri = format!("http://127.0.0.1:{}/path", addr.port());
+    let uri = format!("http://127.0.0.1:{}/path1", addr.port());
 
     let req = http::Request::get(&uri).body(())?;
     let res = req.send().block()?;
 
     assert_eq!(res.status_code(), 200);
-    assert_eq!(
-        res.header("content-type"),
-        Some("text/html; charset=ISO-8859-1")
-    );
-
-    let bytes = res.into_body().read_to_vec().block()?;
-    assert!(bytes.len() > 100);
+    let body = res.into_body().read_to_string().block()?;
+    assert_eq!(body, "OK");
 
     shut.shutdown().block();
     Ok(())
