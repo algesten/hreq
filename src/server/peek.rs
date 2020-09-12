ab8d0c3e62a02142c2dafb650ee0a3745865ac02
Martin Algesten <martin@algesten.se>
2020-09-12 17:45:15 +0200

    More efficient Peekable

--- src/server/peek.rs
+++ src/server/peek.rs
@@ -8,6 +8,8 @@ use std::task::{Context, Poll};
 pub(crate) struct Peekable<S> {
     stream: S,
     buf: Vec<u8>,
+    idx: usize,
+    finished: bool,
 }
 
 impl<S> Peekable<S> {
@@ -15,19 +17,51 @@ impl<S> Peekable<S> {
         Peekable {
             stream,
             buf: Vec::with_capacity(capacity),
+            idx: 0,
+            finished: false,
         }
     }
 }
 
 impl<S: AsyncRead + Unpin> Peekable<S> {
     pub async fn peek(&mut self, len: usize) -> Result<&[u8], io::Error> {
-        let cur = self.buf.len();
-        if cur >= len {
-            return Ok(&self.buf[0..len]);
+        // peeking will reset the read index if we have one.
+        if self.idx > 0 {
+            let split = self.buf.split_off(self.idx);
+            self.buf = split;
+            self.idx = 0;
         }
-        self.buf.resize(len, 0);
-        self.stream.read_exact(&mut self.buf[cur..]).await?;
-        Ok(&self.buf[0..len])
+
+        // ensure we can hold more elements
+        self.buf.reserve(len - self.buf.len());
+
+        loop {
+            let cur_len = self.buf.len();
+
+            if cur_len >= len || self.finished {
+                let to_return = cur_len.min(len);
+
+                return Ok(&self.buf[0..to_return]);
+            }
+
+            // we will set the size down again once read.
+            unsafe { self.buf.set_len(len) };
+
+            let x = self.stream.read(&mut self.buf[cur_len..]).await;
+
+            let amt = if let Ok(amt) = &x { *amt } else { 0 };
+
+            // must always run to not have unitialized bytes in buf.
+            let new_len = cur_len + amt;
+            self.buf.truncate(new_len);
+
+            // error exit here.
+            let read = x?;
+
+            if read == 0 {
+                self.finished = true;
+            }
+        }
     }
 }
 
@@ -38,15 +72,23 @@ impl<S: AsyncRead + Unpin> AsyncRead for Peekable<S> {
         buf: &mut [u8],
     ) -> Poll<io::Result<usize>> {
         let this = self.get_mut();
-        if !this.buf.is_empty() {
-            let max = this.buf.len().min(buf.len());
-            (&mut buf[0..max]).copy_from_slice(&this.buf[0..max]);
-            // TODO: this is not efficient, if we were making a general purpose
-            // peekable we would have some index into self.buf to indicate how
-            // much of the buffer has been read. however, we are only expecting
-            // to read the buf once (for peeking the http2 preface).
-            let split = this.buf.split_off(max);
-            this.buf = split;
+
+        let left = this.buf.len() - this.idx;
+
+        if left > 0 {
+            // max amount we can read from the peeked bytes
+            let max = left.min(buf.len());
+
+            (&mut buf[0..max]).copy_from_slice(&this.buf[this.idx..(this.idx + max)]);
+
+            this.idx += max;
+
+            if this.idx == this.buf.len() {
+                // fully read
+                this.buf.clear();
+                this.idx = 0;
+            }
+
             return Ok(max).into();
         }
         Pin::new(&mut this.stream).poll_read(cx, buf)
@@ -90,6 +132,7 @@ impl<S: AsyncSeek + Unpin> AsyncSeek for Peekable<S> {
 
         // repositioning dumps buffered content
         this.buf.clear();
+        this.idx = 0;
 
         Pin::new(&mut this.stream).poll_seek(cx, pos)
     }
