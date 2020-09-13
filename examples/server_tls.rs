f0f672b73abe78a38f61afe27f852088983a5e1f
Martin Algesten <martin@algesten.se>
2020-09-13 13:25:27 +0200

    TLS server example

---
+++ examples/server_tls.rs
@@ -0,0 +1,25 @@
+use hreq::prelude::*;
+
+pub fn main() {
+    let mut server = Server::new();
+
+    server
+        .at("/*any")
+        .all(|req: http::Request<Body>| async move {
+            if let Some(v) = req.into_body().read_to_vec().await.ok() {
+                format!("You sent: {} bytes\n", v.len())
+            } else {
+                "Nothing sent".into()
+            }
+        });
+
+    let config = hreq::server::TlsConfig::new()
+        .key_path("tests/data/tls_cert.pem")
+        .cert_path("tests/data/tls_cert.pem");
+
+    let (handle, addr) = server.listen_tls(3000, config).block().unwrap();
+
+    println!("TLS listening to: {}", addr);
+
+    handle.keep_alive().block();
+}
--- tests/tls.rs
+++ tests/tls.rs
@@ -48,6 +48,7 @@ fn tls_req_body100mb_with_size() -> Result<(), hreq::Error> {
             assert_eq!(req.header("transfer-encoding"), None);
             assert_eq!(req.header_as("content-length"), Some(SIZE));
             assert_eq!(req.header("content-encoding"), None);
+            assert_eq!(req.header("content-type"), Some("application/octet-stream"));
 
             let v = req.into_body().read_to_vec().await.unwrap();
             assert_eq!(v.len(), SIZE as usize);
